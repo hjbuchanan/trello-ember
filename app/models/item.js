dce664baaccbb73b0a9bcd28ace9a7ce9fd31104
Hannah Jane  Buchanan <hannahjanebuchanan@Hannahs-MacBook-Pro.local>
2016-06-05 12:37:33 -0400

    initial commit

---
+++ app/models/item.js
@@ -0,0 +1,6 @@
+import Model from 'ember-data/model';
+
+export default Model.extend({
+  title: attr(),
+  description: attr()
+});
---
+++ mirage/config.js
@@ -0,0 +1,58 @@
+export default function() {
+  this.get('/lists', function() {
+    return { 
+      "data": [{
+        "type": "lists",
+        "id": 1,
+        "title": "grocery list",
+        "description": "my lists for groceries"
+      }, {
+        "type": "lists",
+        "id": 2, 
+        "title": "todo",
+        "description": "my todos for today"
+      }]
+    };
+  });
+
+  this.get('/items', function() {
+    return { 
+      "data": [{
+        "type": "items",
+        "id": 1,
+        "list-id": 1,
+        "title": "bell peppers",
+        "description": "for cooking"
+      }, {
+        "type": "items",
+        "id": 2,
+        "list-id": 1, 
+        "title": "chicken",
+        "description": "for soup"
+      }]
+    };
+  })
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Shorthand cheatsheet:
+
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+
+    http://www.ember-cli-mirage.com/docs/v0.2.0-beta.7/shorthands/
+  */
+}
