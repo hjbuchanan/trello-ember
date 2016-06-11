dce78d0e0a3322b40c2bd75b70a49cfe374d96d5
Hannah Jane  Buchanan <hannahjanebuchanan@Hannahs-MacBook-Pro.local>
2016-06-11 11:10:16 -0400

    feature(): confirm delete item

--- app/components/list-item.js
+++ app/components/list-item.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	// will need a detail view
 	showDetailModal: 'none',
+	showConfirmDelete: 'none',
 	actions: {
 		clickedList() {
 			this.set('showDetailModal', true);
@@ -12,8 +13,19 @@ export default Ember.Component.extend({
 			this.set('showDetailModal', 'none');
 		},
 
+		// TODO: need to confirm the delete
 		deleteListItem(item, list) {
 			this.sendAction('deleteListItem', item, list);
+		},
+
+		confirmDelete() {
+			this.set('showDetailModal', 'none');
+			this.set('showConfirmDelete', true);
+		},
+
+		closeDeleteModal() {
+			this.set('showConfirmDelete', 'none');
+			// this.set('showDetailModal', 'none');
 		}
 	}
 
